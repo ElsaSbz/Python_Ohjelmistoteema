ac41c4ead89cdad2423e4a011127709142b3bdc1
elsa <sabzalishadi@gmail.com>
2025-08-22 17:27:38 +0300

    ex 4 module 2

---
+++ Module 2/Exercise4.py
@@ -0,0 +1,8 @@
+# Write a program that asks the user for three integer numbers. The program prints out the sum, product, and average of the
+# numbers.
+first_number = int(input("Enter the first integer number: "))
+second_number = int(input("Enter the second integer number: "))
+third_number = int(input("Enter the third integer number: "))
+print("The sum is", first_number+second_number+third_number)
+print("The product is", first_number*second_number*third_number)
+print ("The avarage is", (first_number+second_number+third_number)/3)
