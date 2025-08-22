fab87b5dc945d9a3ba984ca0f8e1dd0e1388b61b
elsa <sabzalishadi@gmail.com>
2025-08-22 13:02:51 +0300

    name edited and exercise 2

---
+++ Module 2/Exercise2.py
@@ -0,0 +1,8 @@
+# Write a program that asks the user for the radius of a circle and the prints out the area of the circle.
+
+import math
+
+r = int(input("Enter the radius of the circle: "))
+area = r*r*math.pi
+print("The area of the circle is", area)
+
