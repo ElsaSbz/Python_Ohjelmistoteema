124f2c1dadc2ae055f7bd0fae8f6b992769faab8
elsa <sabzalishadi@gmail.com>
2025-09-05 09:38:51 +0300

    module 6 ex:4

--- module6/exercise4.py
+++ module6/exercise4.py
@@ -1,2 +1,7 @@
-# Write a function that gets a list of integers as a parameter. The function returns the sum of all the numbers in the list. For testing, write a main program where you create a list, call the function, and print out the value it returned.
+# Write a function that gets a list of integers as a parameter.
+# The function returns the sum of all the numbers in the list. For testing, write a main program where you create a list, call the function, and print out the value it returned.
 
+def sum_list (numbers):
+    return sum(numbers)
+list = [1,2,3,4,5,6,7,8,9]
+print(sum_list(list))
