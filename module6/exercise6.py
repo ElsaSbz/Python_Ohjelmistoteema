b62064c8167cc5c7ae3f3357b54502fcae546797
elsa <sabzalishadi@gmail.com>
2025-09-05 11:03:43 +0300

    module 6 ex:6

--- module6/exercise6.py
+++ module6/exercise6.py
@@ -1,2 +1,20 @@
-# Write a function that receives two parameters: the diameter of a round pizza in centimeters and the price of the pizza in euros. The function calculates and returns the unit price of the pizza per square meter. The main program asks the user to enter the diameter and price of two pizzas and tells the user which pizza provides better value for money (which of them has a lower unit price). You must use the function you wrote for calculating the unit prices.
+# Write a function that receives two parameters: the diameter of a round pizza in centimeters and the price of the pizza in euros.
+# The function calculates and returns the unit price of the pizza per square meter.
+# The main program asks the user to enter the diameter and price of two pizzas and tells the user which pizza provides better value for money (which of them has a lower unit price).
+# You must use the function you wrote for calculating the unit prices.
 
+def pizza ( round , price) :
+    round_m = round / 100
+    return price / round_m
+first_round = float(input("Enter the first pizza's round in centimeter: "))
+first_price = float(input("Enter the first pizza's price in euro: "))
+first_value = pizza ( round = first_round, price = first_price)
+second_round = float(input("Enter the second pizza's round in centimeter: "))
+second_price = float(input("Enter the second pizza's price in euro: "))
+second_value = pizza ( round = second_round, price = second_price)
+if first_value < second_value:
+    print("The first pizza is better than the second pizza")
+elif first_value > second_value:
+    print("The second pizza is better than the first pizza")
+else:
+    print("Both pizza have the same value")
