ab19badcd12254aadc14379050c0270bb6496ea1
elsa <sabzalishadi@gmail.com>
2025-11-08 13:51:47 +0200

    software 2 , ex1 , 2

---
+++ Software 2/Module1/Assignment 2/ex2.js
@@ -0,0 +1,4 @@
+//Write a program that prompts for user's name and then greets the user. Print the result to the HTML document: Hello, Name!
+const name =prompt('Type your name.');
+document.querySelector('#target').innerHTML = 'Hello, ' + name + '!';
+console.log('Hello, ' + name);
